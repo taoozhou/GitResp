615a7c8721d450312e37ad3e9c2a08d741591660
ZhouT <stu_ZhouTao@outlook.com>
2021-02-25 18:35:26 +0800

    给Person添加name属性

--- src/com/zhout/git/Person.java
+++ src/com/zhout/git/Person.java
@@ -1,10 +1,12 @@
 package com.zhout.git;
-
+//GitHub的账号：Tao-Zhou-Student
+//GitHub的密码：kxRJy:bn!v5nDwt
 /**
  * @Author ZhouT
  * @Date 2021/2/25 18:26
  */
 public class Person {
     private int age ;
-    
+    private int name ;
+
 }
